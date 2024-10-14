5f9ca6fa83a8f4fcc44dd034dabea30f16ce56c8
benard <benardboiwo703@gmail.com>
2024-10-14 13:05:36 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// index.js
+
+// Function expression for divide
+function divide(a, b) {
+    return a / b;
+}
+
+// Arrow function for square
+const square = (x) => {
+    return x * x;
+};
+
+// Arrow function for add
+const add = (x, y) => {
+    return x + y;
+};
